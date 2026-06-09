61c126169a5af40f6d07ca5d9eabc05aaed1088e
bamboocharacing <max.holder@bamboocharacing.de>
2026-06-09 12:55:20 +0200

    Fix syntax error in architecture detection for ARM64

--- hlp4win11.ps1
+++ hlp4win11.ps1
@@ -225,7 +225,7 @@ if ($architecture -eq 'AMD64') {
 } elseif ($architecture -eq 'x86') {
     $archKey = 'x86'
     Write-Host "  Detected architecture: x86" -ForegroundColor Green
-} elseif ($architecture -eq 'ARM64' {
+} elseif ($architecture -eq 'ARM64') {
    $archKey = 'x64'
    Write-Host "  Detected architecture: ARM64 - Falling to x64 binaries." -ForegroundColor Green
 } else {
